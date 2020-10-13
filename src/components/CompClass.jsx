043f775ba72f112246b840e9b2c5e54f7ca6b8b6
Costa <matheus.costa.ol97@gmail.com>
2020-10-13 20:46:05 -0300

    inicio de class

---
+++ src/components/CompClass.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Component } from 'react';
+
+// import React, { Component } from 'react';
+
+export default class CompClass extends Component {
+    
+}
--- src/index.jsx
+++ src/index.jsx
@@ -14,6 +14,41 @@ ReactDOM.render(
         <Member name='Kleyd' lastName='Bar'/>
         <Member name='Gus' lastName='Tavo'/>
         <Member name='Ander' lastName='Naka'/>
+        <div>
+              <ul type="square">
+                    <li>item 1</li>
+                    <li>item 2</li>
+                    <li>item 3</li>
+                    <li>item 4</li>
+                    <li>item 5</li>
+                    <li>item 6</li>
+              </ul>
+              <ul type="circle">
+                    <li>item 7</li>
+                    <li>item 8</li>
+                    <li>item 9</li>
+                    <li>item 10</li>
+                    <li>item 11</li>
+                    <li>item 12</li>
+              </ul>
+              <table>
+                  <tr>
+                        <th>\Firstname/</th>
+                        <th>\Lastname/</th>
+                        <th>\Age/</th>
+                  </tr>
+                  <tr>
+                        <td>Jill</td>
+                        <td>Smith</td>
+                        <td>50</td>
+                  </tr>
+                  <tr>
+                        <td>Eve</td>
+                        <td>Jackson</td>
+                        <td>94</td>
+                  </tr>
+            </table>
+        </div>
   </Family>,
    document.getElementById('root')
 );
